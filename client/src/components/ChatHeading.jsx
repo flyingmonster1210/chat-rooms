ed10a6d6dd4638eb4330a5fa37f76b9cedb41da1
Wayne <zweijie1210@outlook.com>
2023-10-12 15:05:32 -0700

    Sign out is done

--- client/src/components/ChatHeading.jsx
+++ client/src/components/ChatHeading.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import shutdown from '../assets/shutdown.png'
+import { useNavigate } from 'react-router-dom'
 
 function ChatHeading({ selectedUser }) {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/login', { replace: true })
+  }
+
   return (
     <div
       className={`flex w-full p-1 items-center h-[3rem] ${
@@ -22,6 +29,7 @@ function ChatHeading({ selectedUser }) {
         src={shutdown}
         alt="Shutdown"
         title="Logout"
+        onClick={handleClick}
         className="rounded-lg h-[1.8rem] hover:cursor-pointer"
       />
     </div>
