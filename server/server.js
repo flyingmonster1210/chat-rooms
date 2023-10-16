56bed31c400f8d900544b8f1c505bb3c55169e2a
Wayne <zweijie1210@outlook.com>
2023-10-16 13:33:11 -0700

    Initialize socket in server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const socket = require('socket.io')
 const connectDB = require('./db')
 const colors = require('colors')
 const requestInfo = require('./middlewares/normal')
@@ -7,12 +8,39 @@ const dotenv = require('dotenv').config()
 
 const port = process.env.PORT
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('\n\n-----------------------------------------------------------')
   console.log(`Server is listening on PORT:${port}`.green)
 })
 
 connectDB()
+
+const io = socket(server, {
+  cors: {
+    origin: 'http://localhost:5050',
+    credentials: true,
+  }
+})
+
+global.onlineUsers = new Map()
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  global.chatSocket = socket
+
+  // Once the user login, put the userId into onlineUsers map
+  socket.on('add-user', (userId) => {
+    onlineUsers.set(userId, socket.id)
+  })
+
+  // Once the user send a message, send the message to the user
+  // data = {to: userId, message: msg}
+  socket.on('send-message', (data) => {
+    const recipient = onlineUsers.get(data.to)
+    if (recipient) {
+      socket.to(recipient).emit('message-recieve', data.message)
+    }
+  })
+})
 
 app.use(requestInfo)
 
